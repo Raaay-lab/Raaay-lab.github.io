1cb1d3e444bb2f1c497b2904c5de77c546c8c369
Raaay-lab <63502980+Raaay-lab@users.noreply.github.com>
2020-04-13 22:27:46 +0400

    second sammary

---
+++ lab02_ip/Новости.php
@@ -0,0 +1,125 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset = "utf8">
+<script src="js/jquery-3.4.1.slim.min.js"></script>
+<script src="js/popper.min.js"></script>
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<script src="js/bootstrap.min.js"></script>
+<link rel="stylesheet" type="text/css" href="css/stylesNews.css">
+<style>
+
+</style>
+<title>Новости</title>
+</head>
+<body bgcolor="#151616">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="Главная.php"><img src = "images/RDU.jpg" width = "127px" height = "45px"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="Главная.php">Главная<span class="sr-only">(current)</span></a>
+      </li>
+      
+	  <li class="nav-item active">
+        <a class="nav-link" href="Новости.php">Новости<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Как с нами связаться
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="pasha-zakharov-14@mail.ru">Почта</a>
+          <a class="dropdown-item" href="https://vk.com/rducyberclub">Вконтакте</a>
+		  <a class="dropdown-item" href="#">Телеграмм</a>
+		  <a class="dropdown-item" href="#">+79998887766</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+	        <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Информация
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Желающим поступить</a>
+          <a class="dropdown-item" href="#">СМИ</a>
+		  <a class="dropdown-item" href="#">Родителям</a>
+		  <a class="dropdown-item" href="#">Организациям</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+	  	        <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Важные ссылки
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="https://liquipedia.net/counterstrike/Main_Page">Liquipedia</a>
+          <a class="dropdown-item" href="http://rducyberclub.com">Сайт Клуба</a>
+		  <a class="dropdown-item" href="https://www.hltv.org/">HLTV</a>
+        </div>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <button class="buttonNew" type="submit">Хочу поступить</button>
+    </form>
+  </div>
+</nav>
+<div class = "list">
+<div class = "MenuMiddle">
+<div class = "okno"><a type="button" class="buttonNew" href="Турниры.php">Турниры</a></div>
+<div class = "okno"><a type="button" class="buttonNew" href="О_ВУЗе.php">O RDU</a></div>
+<div class = "okno"><a type="button" class="buttonNew" href="Направления.php">Направления</a></div>
+<div class = "okno"><a type="button" class="buttonNew" href="Преподаватели.php">Преподаватели</a></div>
+<div class = "okno"><a type="button" class="buttonNew" href="Выпускники.php">Выпускники</a></div>
+</div>
+<div class = "zag">Новости</div>
+<a href = "" class = "block1" style = "color:white;text-decoration:none;">
+	<div class = "news1"><p><img src = "images/1.png" alt = "" class = "f1">
+	Выбор игроков без организации для вашей фэнтезийной команды оказался решающим для успеха,
+	поскольку канадско-американская команда продолжала обеспечивать себе место в первом сезоне FLASHPOINT над такими,
+	как HAVU и Chaos. Ведущим был Гейдж "Infinite" Грин, а также Ясин "Subroza" Тауфик, который в итоге стал вторым лучшим
+	исполнителем фэнтези, несмотря на его скромный ценник в 176000 долларов.</p></div>
+</a>
+<a href = "#" class = "block1" style = "color:white;text-decoration:none;">
+	<div class = "news1"><p><img src = "images/2.png" alt = "" class = "f2">
+	27-летний игрок надеется, что переход позволит ему снова найти свое место после "ужасных" шести месяцев, в течение которых
+	он чувствовал, что он был недостаточно эффективен (рейтинг 0,96 LAN). Он возьмет на себя роль Алекса, который официально ушел из
+	Vitality в среду после того, как пришел к выводу, что его усилия облегчить график поездок команды будут бесплодными.</p></div>
+</a>
+	<a href = "#" class = "block1" style = "color:white;text-decoration:none;">
+	<div class = "news1"><p><img src = "images/3.png" alt = "" class = "f3">Во втором месяце 2020 года состоялись три международных мероприятия:
+	BLAST Premier Spring 2020, DreamHack Open Anaheim и IEM Katowice. После их завершения рейтинги сильно и зменились, со значительными 
+	кoрректировками в  топ-10, с устоявшимися списками тонущих и командами на подъеме, продолжающими свое восхождение. Первое место рейтинга команд по версии HLTV.org
+	стали занимать NA`Vi.</p></div>
+</a>
+	<a href = "#" class = "block1" style = "color:white;text-decoration:none;">
+	<div class = "news1"><p><img src = "images/4.png" alt = "" class = "f4">Новости подтверждают сообщение вторника: 1pv.fr 
+	, который впервые показал, что британский игрок собирался покинуть витальность после того, как устал в результате неустанного графика поездок.
+	В отдельном заявлении лидер игры поделился тем, что команда отсутствовала дома в течение 36 недель в прошлом году, которые они надеялись сократить
+	до 22 недель в 2020 году. </p></div>
+</a>
+	<a href = "#" class = "block1" style = "color:white;text-decoration:none;">
+	<div class = "news1"><p><img src = "images/5.png" alt = "" class = "f5">Отборочный процесс для несовершеннолетних продолжится позже на этой неделе 
+	с закрытым этапом, показывающим двойное исключение, лучший из трех скобок. Половина из 96 мест была заполнена приглашениями, а другая половина досталась 
+	ведущим командам открытого этапа, который проходил с 31 января по 16 февраля.</p></div>
+</a>
+	<a href = "#" class = "block1" style = "color:white;text-decoration:none;">
+	<div class = "news1"><p><img src = "images/6.png" alt = "" class = "f6">Дебют бума с их свежеиспеченным составом был испорчен на ранней стадии в закрытом 
+	отборочном турнире Южной Америки, когда команда оказалась отброшенной в нижнюю сетку в своем стартовом матче, упав на TeamOne в двух близко оспариваемых
+	картах. Восстановление бразильской команды после первоначального поражения было обеспечено звездным шоу от их нового игрока</p></div>
+</a>
+ <div class = "copy">
+ © Made by Raaay<br>
+ all rights reserved
+ </div>
+ </div>
+</body>
+</html>
