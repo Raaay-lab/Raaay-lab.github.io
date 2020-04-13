db5d348974e9a7b9562e8503379f44e4bf006e85
Raaay-lab <63502980+Raaay-lab@users.noreply.github.com>
2020-04-13 22:32:47 +0400

    kuku

---
+++ RDU/Турниры.php
@@ -0,0 +1,221 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset = "utf8">
+<script src="js/jquery-3.4.1.slim.min.js"></script>
+<script src="js/popper.min.js"></script>
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<script src="js/bootstrap.min.js"></script>
+<link rel="stylesheet" type="text/css" href="css/stylesTournaments.css">
+<title>Турниры</title>
+</head>
+<body>
+<div class = "topl" >
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="Главная.php"><img src = "images/RDU.jpg" width = "127px" height = "45px"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="Главная.php">Главная</a>
+      </li>
+      
+	  <li class="nav-item">
+        <a class="nav-link" href="Новости.php">Новости</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Как с нами связаться
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="pasha-zakharov-14@mail.ru">Почта</a>
+          <a class="dropdown-item" href="https://vk.com/rducyberclub">Вконтакте</a>
+		  <a class="dropdown-item" href="#">Телеграмм</a>
+		  <a class="dropdown-item" href="#">+79998887766</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+	        <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Информация
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Желающим поступить</a>
+          <a class="dropdown-item" href="#">СМИ</a>
+		  <a class="dropdown-item" href="#">Родителям</a>
+		  <a class="dropdown-item" href="#">Организациям</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+	  	        <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Важные ссылки
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="https://liquipedia.net/counterstrike/Main_Page">Liquipedia</a>
+          <a class="dropdown-item" href="http://rducyberclub.com">Сайт Клуба</a>
+		  <a class="dropdown-item" href="https://www.hltv.org/">HLTV</a>
+        </div>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <button class="btn buttonNew" type="submit">Хочу поступить</button>
+    </form>
+  </div>
+</nav>
+<div class = "list">
+<div class = "MenuMiddle">
+
+<div class = "okno"><a type="button" class="buttonNew active kek" href="Турниры.php">Турниры</a></div>
+<div class = "okno"><a type="button" class="buttonNew" href="О_ВУЗе.php">O RDU</a></div>
+<div class = "okno"><a type="button" class="buttonNew" href="Направления.php">Направления</a></div>
+<div class = "okno"><a type="button" class="buttonNew" href="Преподаватели.php">Преподаватели</a></div>
+<div class = "okno"><a type="button" class="buttonNew" href="Выпускники.php">Выпускники</a></div>
+</div>
+
+<div class = "event-holder">
+	<div class = "otstyp">
+	</div>
+	<div class = "event_month">
+		<div class = "headline">Mart 2020
+		</div>
+		<div class "bigevents">
+		<a href ="#" class = "turic1">
+		<div class = "eventphoto"><img src = "images/турнир1.jpeg" alt = "" style = "height:77px;width: 314px; border-radius:10px 10px 10px 10px;"></div>
+		<div class = "information">
+			<div class = "info">
+				<div class = "tima">ESL One Rio 2020 Minor Play-in</div>
+				<div class = "location">Malta, EU & Denver, USA</div>
+			</div>
+			<div class = "information2">
+<table class="table">
+             <tbody><tr>
+               <td class="col-value col-date"><span class="" data-time-format="MMM do" data-unix="1584356400000">Mar 16th</span><span> - <span class="" data-time-format="MMM do" data-unix="1586685600000">Apr 12th</span></span></td>
+               <td class="col-value" title="$750,000">$750,000</td>
+               <td class="col-value">24</td>
+             </tr>
+             <tr>
+              <td class="col-desc col-date">Date</td>
+              <td class="col-desc">Prize</td>
+              <td class="col-desc">Teams</td>
+            </tr>
+          </tbody>
+</table>
+			</div>
+		</div>
+		</a>
+		</div>
+	</div>
+	<div class = "otstyp">
+	</div>	
+	<div class = "event_month">
+		<div class = "headline">April 2020
+		</div>
+		<div class "bigevents">
+		<a href ="#" class = "turic1">
+		<div class = "eventphoto"><img src = "images/турнир2.jpeg" alt = "" style = "height:77px;width: 314px;border-radius:10px 10px 10px 10px;"></div>
+				<div class = "information">
+			<div class = "info">
+				<div class = "tima">Copenhagen Games 2020</div>
+				<div class = "location">Copenhagen, Denmark</div>
+			</div>
+			<div class = "information2">
+			<table class="table">
+            <tbody><tr>
+              <td class="col-value col-date"><span class="" data-time-format="MMM do" data-unix="1586340000000">Apr 8th</span><span> - <span class="" data-time-format="MMM do" data-unix="1586599200000">Apr 11th</span></span></td>
+              <td class="col-value" title="$73,792">$73,792</td>
+              <td class="col-value">TBA</td>
+            </tr>
+            <tr>
+              <td class="col-desc col-date">Date</td>
+              <td class="col-desc">Prize</td>
+              <td class="col-desc">Teams</td>
+            </tr>
+          </tbody></table>
+			</div>
+		</div>
+		</a>
+		</div>
+	</div>
+	<div class = "otstyp">
+	</div>	
+	<div class = "event_month">
+		<div class = "headline">May 2020
+		</div>
+		<div class "bigevents">
+		<a href ="#" class = "turic1">
+		<div class = "eventphoto"><img src = "images/турнир3.jpeg" alt = "" style = "height:77px;width: 314px;border-radius:10px 10px 10px 10px;"></div>
+				<div class = "information">
+			<div class = "info">
+				<div class = "tima">ESL One Rio 2020</div>
+				<div class = "location">Rio de Janeiro, Brazil</div>
+			</div>
+			<div class = "information2">
+			<table class="table">
+             <tbody><tr>
+               <td class="col-value col-date"><span class="" data-time-format="MMM do" data-unix="1589623200000">May 16th</span><span> - <span class="" data-time-format="MMM do" data-unix="1590314400000">May 24th</span></span></td>
+               <td class="col-value" title="$1,000,000">$1,000,000</td>
+               <td class="col-value">16</td>
+             </tr>
+             <tr>
+              <td class="col-desc col-date">Date</td>
+              <td class="col-desc">Prize</td>
+              <td class="col-desc">Teams</td>
+            </tr>
+              </tbody></table>
+			</div>
+		</div>
+		</a>
+
+		</div>
+	</div>
+	<div class = "otstyp">
+	</div>	
+	<div class = "event_month">
+		<div class = "headline">June 2020
+		</div>
+		<div class "bigevents">
+		<a href ="#" class = "turic1">
+		<div class = "eventphoto"><img src = "images/турнир4.jpeg" alt = "" style = "height:77px;width: 314px;border-radius:10px 10px 10px 10px;"></div>
+				<div class = "information">
+			<div class = "info">
+				<div class = "tima">DreamHack Masters Jönköping 2020</div>
+				<div class = "location">Jönköping, Sweden</div>
+			</div>
+			<div class = "information2">
+			<table class="table">
+             <tbody><tr>
+               <td class="col-value col-date"><span class="" data-time-format="MMM do" data-unix="1591696800000">Jun 9th</span><span> - <span class="" data-time-format="MMM do" data-unix="1592128800000">Jun 14th</span></span></td>
+               <td class="col-value" title="$250,000">$250,000</td>
+               <td class="col-value">16</td>
+             </tr>
+             <tr>
+               <td class="col-desc col-date">Date</td>
+               <td class="col-desc">Prize</td>
+               <td class="col-desc">Teams</td>
+             </tr>
+             </tbody></table>
+			</div>
+		</div>
+		</a>
+		</div>
+	</div>
+	<div class = "otstyp">
+	</div>	
+</div>
+<div class = "otstyp">
+</div>	
+ <div class = "copy">
+ © Made by Raaay<br>
+ all rights reserved
+ </div>
+ </div>
+</body>
+</html>
