eefc5129c338d0855ea14b097fcb580dace61139
Raaay-lab <63502980+Raaay-lab@users.noreply.github.com>
2021-04-11 22:49:06 +0400

    Update script1.js

--- tttttttttttttt/core/js/script1.js
+++ tttttttttttttt/core/js/script1.js
@@ -1,7 +1,7 @@
 var slides1 = document.querySelectorAll('.pic1');
 var massIm1 = [];
 var dl1 = slides1.length;
-var platon1 = "Raaay-lab.github.io/philosophy/core/index5.html";
+var platon1 = "Raaay-lab.github.io/philosophy/core/index5.html" ;
 var avreliy1 = "Raaay-lab.github.io/philosophy/core/index1.html";
 
 console.log(slides1)
